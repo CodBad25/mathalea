1153f073816bf6433f1f5742117d5e7c5c47f981
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2026-02-12 08:14:53 +0100

    3F20-4 et 2F10-9 passe en fillInTheBlank à un seul mathfield par question pour compatibilité CAN

--- src/exercices/2e/2F10-9-old.ts
+++ src/exercices/2e/2F10-9-old.ts
@@ -1,14 +1,14 @@
-import CoefficientDirecteur from '../3e/3F20-4'
+import CoefficientDirecteur from '../3e/3F20-4-old'
 export const titre =
   "Reconnaitre coefficient directeur et ordonnée à l'origine d'une fonction affine"
 export const dateDePublication = '19/05/2025'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 
-export const uuid = '80d4e'
+export const uuid = '80d4f'
 export const refs = {
-  'fr-fr': ['2F10-9'],
-  'fr-ch': ['1mF1-13', '10FA5-18', '11FA8-15'],
+  'fr-fr': [],
+  'fr-ch': [],
 }
 export default class CoefficientDirecteur2nde extends CoefficientDirecteur {
   constructor() {
