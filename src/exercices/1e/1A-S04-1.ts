6667591765f62abb977b3c0eb35b3ec3b13c34d9
gillesMora <gilles.mora@ac-normandie.fr>
2026-02-22 11:28:36 +0100

    modif ref auto Stat

--- src/exercices/1e/1A-S04-1.ts
+++ src/exercices/1e/1A-S04-1.ts
@@ -1,88 +1,72 @@
 import Stat from '../../lib/mathFonctions/Stat'
 import { choice } from '../../lib/outils/arrayOutils'
+import { miseEnEvidence } from '../../lib/outils/embellissements'
 import { texNombre } from '../../lib/outils/texNombre'
+import { randint } from '../../modules/outils'
 import { creerSerieDeMoyenneEtEtendue } from '../../modules/outilsStat'
 import { nombreElementsDifferents } from '../ExerciceQcm'
 import ExerciceQcmA from '../ExerciceQcmA'
 
-export const uuid = '4ba2a'
+export const uuid = '3ba7a'
 export const refs = {
-  'fr-fr': ['1A-S02-13'],
+  'fr-fr': ['1A-S04-1'],
   'fr-ch': [],
 }
 export const interactifReady = true
 export const interactifType = 'qcm'
 export const amcReady = 'true'
 export const amcType = 'qcmMono'
-export const titre = "Calculer le pourcentage d'élèves obtenant la moyenne"
+export const titre = "Calculer un effectif total à partir d'un diagramme"
 export const dateDePublication = '31/12/2025'
 /**
  * @author Jean-claude Lhote
  */
 
-export default class CalculPourcentageEtMoyenneQCM extends ExerciceQcmA {
+export default class CalculEffectifQCM extends ExerciceQcmA {
   // Ceci est la fonction qui s'occupe d'écrire l'énoncé, la correction et les réponses
   // Elle factorise le code qui serait dupliqué dans versionAleatoire et versionOriginale
   private appliquerLesValeurs(
     serie: [number, number][] | number[],
     noteMax: number,
   ): void {
-    const valeurCible = noteMax / 2
     const maSerie = new Stat(serie)
     const histogramme = maSerie.diagramme({
       cumul: false,
       barres: true,
       valuesOn: true,
     })
-    const effectifCible = maSerie.serieTableau
-      .filter(([note]) => Number(note) >= valeurCible)
-      .reduce((acc, [, effectif]) => acc + effectif, 0)
-    const n = maSerie.serie.length
 
-    const pourCent = Math.round((effectifCible / n) * 100)
-    const distracteur1 = choice(
-      [pourCent - 10, pourCent + 10, pourCent - 15, pourCent + 15].filter(
-        (v) => v > 0 && v < 100,
-      ),
-      pourCent,
-    )
-    const distracteur2 = choice(
-      [pourCent - 5, pourCent + 5, pourCent - 20, pourCent + 20].filter(
-        (v) => v > 0 && v < 100,
-      ),
-      [pourCent, distracteur1],
-    )
-    const distracteur3 = choice(
-      [
-        pourCent - 10,
-        pourCent + 10,
-        pourCent - 15,
-        pourCent + 15,
-        pourCent - 5,
-        pourCent + 5,
-        pourCent - 20,
-        pourCent + 20,
-      ].filter((v) => v > 0 && v < 100),
-      [pourCent, distracteur1, distracteur2],
-    )
+    const n = maSerie.serie.length
+    const listeTableau = maSerie.serieTableau
+
+    const distracteur1 =
+      Math.round(maSerie.serie.length / 4) * 4 !== n
+        ? Math.round(maSerie.serie.length / 4) * 4
+        : Math.round(maSerie.serie.length / 5) * 5 !== n
+          ? Math.round(maSerie.serie.length / 5) * 5
+          : n - 1
+    const distracteur2 = listeTableau.length
+    const distracteur3 = listeTableau
+      .map(([note, effectif]) => Number(note))
+      .reduce((a, b) => a + b)
 
     this.reponses = [
-      `${texNombre(pourCent, 0)}~\\%`,
-      `${texNombre(distracteur1, 0)}~\\%`,
-      `${texNombre(distracteur2, 0)}~\\%`,
-      `${texNombre(distracteur3, 0)}~\\%`,
+      `${texNombre(n, 0)}`,
+      `${texNombre(distracteur1, 0)}`,
+      `${texNombre(distracteur2, 0)}`,
+      `${texNombre(distracteur3, 0)}`,
     ].map((r) => `$${r}$`)
     this.enonce = `Voici la répartition des notes sur ${noteMax} d'une classe de première.<br>
       ${histogramme}<br><br>
-      Quel est le pourcentage d'élèves ayant obtenu la moyenne ?`
+      Quel est l'effectif total de cette classe ?`
 
     // Correction : explication simple, claire
-    this.correction = `Le pourcentage d'élèves ayant obtenu la moyenne est calculé en divisant l'effectif des élèves ayant obtenu une note supérieure ou égale à la moyenne par l'effectif total, puis en multipliant par 100.<br>
-      L'effectif total est le nombre de notes représentées dans l'histogramme.<br>
-      Ici, on trouve un effectif total de $${n}$ élèves.<br>
-      L'effectif des élèves ayant obtenu une note supérieure ou égale à la moyenne est de $${effectifCible}$.<br>
-      $\\dfrac{${effectifCible}}{${n}} \\times 100 = ${pourCent}$.<br>
-      Donc le pourcentage est de $${pourCent}~\\%$.`
+    this.correction = `L'effectif total est le nombre de notes représentées dans l'histogramme.<br>
+      On peut le calculer en additionnant les effectifs de chaque barre.<br>
+      Les effectifs sont : ${maSerie.serieTableau
+        .map(([note, effectif]) => `$${effectif}$ pour la note $${note}$`)
+        .join('<br>')}.<br>
+      Ici, on trouve un effectif total de $${miseEnEvidence(n)}$ élèves.`
   }
 
   versionOriginale: () => void = () => {
@@ -103,7 +87,7 @@ export default class CalculPourcentageEtMoyenneQCM extends ExerciceQcmA {
       const maSerie = creerSerieDeMoyenneEtEtendue({
         mean: noteMax / 2,
         range: noteMax / 2,
-        n: choice([20, 25, 10]),
+        n: randint(24, 30),
         isInteger: true,
         gaussian: true,
       })
--- src/exercices/1e/1A-S04-6.ts
+++ src/exercices/1e/1A-S04-6.ts
@@ -1,87 +1,79 @@
 import Stat from '../../lib/mathFonctions/Stat'
-import { miseEnEvidence } from '../../lib/outils/embellissements'
 import { texNombre } from '../../lib/outils/texNombre'
 import { randint } from '../../modules/outils'
 import { creerSerieDeQuartiles } from '../../modules/outilsStat'
 import { nombreElementsDifferents } from '../ExerciceQcm'
 import ExerciceQcmA from '../ExerciceQcmA'
 
-export const uuid = '0cb7a'
+export const uuid = '5bb2b'
 export const refs = {
-  'fr-fr': ['1A-S02-17'],
+  'fr-fr': ['1A-S04-6'],
   'fr-ch': [],
 }
 export const interactifReady = true
 export const interactifType = 'qcm'
 export const amcReady = 'true'
 export const amcType = 'qcmMono'
-export const titre = "Lire la médiane d'une série sur une boite à moustaches"
+export const titre = "Lire une fréquence à partir d'un histogramme"
 export const dateDePublication = '01/01/2026'
 /**
  * @author Jean-claude Lhote
  */
 
-export default class LireMedianeBoiteMoustachesQCM extends ExerciceQcmA {
+export default class LireFrequenceHistogrammeQCM extends ExerciceQcmA {
   // Ceci est la fonction qui s'occupe d'écrire l'énoncé, la correction et les réponses
   // Elle factorise le code qui serait dupliqué dans versionAleatoire et versionOriginale
   private appliquerLesValeurs(serie: number[]): void {
     const maSerie = new Stat(serie)
-    const moustache = maSerie.traceBoiteAMoustache({
-      size: 10,
-      height: 4,
-      legendeOn: false,
-      valeursOn: true,
-      echelle: 1,
+    const histogramme = maSerie.diagramme({
+      cumul: true,
+      barres: true,
+      valuesOn: true,
+      effectifsOn: false,
+      percentVsEffectifs: true,
     })
+    const q1 = maSerie.q1
+    const q3 = maSerie.q3
+    const mediane = maSerie.mediane()
+    const max = maSerie.max()
 
-    // Réorganiser les réponses pour mettre la bonne en premier
     this.reponses = [
-      texNombre(maSerie.mediane(), 1),
-      texNombre((maSerie.q1 + maSerie.q3) / 2, 1),
-      texNombre(maSerie.q1, 0),
-      texNombre(maSerie.q3, 0),
+      `${texNombre(q3, 0)}`,
+      `${texNombre(q1, 0)}`,
+      `${texNombre(mediane, 0)}`,
+      `${texNombre(max, 0)}`,
     ].map((r) => `$${r}$`)
-    this.enonce = `Une série statistique est résumée par le diagramme en boite ci-dessous, utilisez-le pour donner la valeur de la médiane de cette série.<br>
-      ${moustache}`
+    this.enonce = `${histogramme}<br><br>Quelle est la valeur du 3e quartile de la série statistique étudiée sur ce diagramme des fréquences cumulées croissantes ?`
 
     // Correction : explication simple, claire
-    this.correction = `La médiane est la valeur qui partage la série statistique en deux parties égales.<br>
-      D'après le diagramme en boite, on a $Q_1=${texNombre(maSerie.q1, 1)}$ et $Q_3=${texNombre(maSerie.q3, 1)}$. La médiane se trouve au niveau du trait intermédiaire.<br>
-      La médiane de la série est donc : $${miseEnEvidence(
-        texNombre(maSerie.mediane(), 1),
-      )}$.`
+    this.correction = `Le troisième quartile $Q_3$ est la plus petite valeur de la série statistique pour laquelle au moins $75\\%$ des valeurs sont inférieures ou égales à $Q_3$.<br>
+     Sur ce diagramme des fréquences cumulées croissantes, on lit que la fréquence cumulée atteint $75\\%$ pour la valeur $${texNombre(q3, 0)}$.<br>`
   }
 
   versionOriginale: () => void = () => {
-    const maSerie = creerSerieDeQuartiles({
-      q1: 30,
-      mediane: 40,
-      q3: 55,
-      min: 10,
-      max: 60,
-      n: 40,
-    })
+    const maSerie: number[] = [1, 1, 2, 2, 3, 4, 5, 5, 5, 6, 6, 7, 8]
     this.appliquerLesValeurs(maSerie)
   }
 
   versionAleatoire: () => void = () => {
     const n = 4 // Nombre de réponses différentes souhaitées
     do {
-      const min = randint(1, 4) * 5
-      const q1 = min + randint(1, 4) * 5
-      const tot = randint(5, 8)
-      const a = randint(2, 4)
+      const min = randint(0, 1)
+      const q1 = min + randint(1, 3) / 2
+      const tot = randint(3, 6) / 2
+      const a = randint(1, 3) / 2
       const b = tot / 2 !== a ? tot - a : tot - a + 1
-      const mediane = q1 + a * 5
-      const q3 = mediane + b * 5
-      const max = Math.round(randint(q3 + 5, 100) / 5) * 5
+      const mediane = q1 + a
+      const q3 = mediane + b
+      const max = randint(Math.ceil(q3), 8)
       const maSerie = creerSerieDeQuartiles({
         q1,
         mediane,
         q3,
         min,
         max,
-        n: 40,
+        n: 12,
+        isInteger: true,
       })
       this.appliquerLesValeurs(maSerie)
     } while (nombreElementsDifferents(this.reponses) < n)
