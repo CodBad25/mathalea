80036c9332ee74da36577e9658123f15c2115c51
Gilles Mora <gilles.mora@ac-normandie.fr>
2026-03-06 17:32:24 +0000

    Gilles can6b 2026

--- src/exercices/can/can6a-2026/can6a-2026-Q10.ts
+++ src/exercices/can/can6a-2026/can6a-2026-Q10.ts
@@ -20,42 +20,66 @@ export default class Can20266Q10 extends ExerciceCan {
   enonce(num?: number, cas?: number) {
     if (num == null || cas == null) {
       num = randint(1, 9) * 10
-      cas = choice([1, 2])
+      cas = choice([1, 2, 3, 4])
     }
     const result = num / 10
 
     this.formatChampTexte = KeyboardType.clavierDeBase
     this.formatInteractif = 'fillInTheBlank'
+    this.consigne = 'Complète.'
 
     if (cas === 1) {
-      // Cas 1 : trouver le numérateur  num/100 = .../10
+      // Cas 1 : num/100 = .../10
       this.reponse = {
         champ1: { value: result },
       }
-this.consigne='Complète.'
+
       this.question = `\\dfrac{${num}}{100}=\\dfrac{%{champ1}}{10}`
 
       this.correction = `$\\dfrac{${num}}{100}=\\dfrac{${miseEnEvidence(result)}}{10}$ (en divisant par $10$ le numérateur et le dénominateur).`
 
       this.canEnonce = 'Complète.'
       this.canReponseACompleter = `$\\dfrac{${num}}{100}=\\dfrac{\\ldots}{10}$`
-    } else {
-      // Cas 2 : trouver le dénominateur  num/100 = result/...
+    } else if (cas === 2) {
+      // Cas 2 : num/100 = result/...
       this.reponse = {
         champ1: { value: 10 },
       }
-this.consigne='Complète.'
+
       this.question = `\\dfrac{${num}}{100}=\\dfrac{${result}}{%{champ1}}`
 
       this.correction = `$\\dfrac{${num}}{100}=\\dfrac{${result}}{${miseEnEvidence(10)}}$ (en divisant par $10$ le numérateur et le dénominateur).`
 
       this.canEnonce = 'Complète.'
       this.canReponseACompleter = `$\\dfrac{${num}}{100}=\\dfrac{${result}}{\\ldots}$`
+    } else if (cas === 3) {
+      // Cas 3 : result/10 = .../100
+      this.reponse = {
+        champ1: { value: num },
+      }
+
+      this.question = `\\dfrac{${result}}{10}=\\dfrac{%{champ1}}{100}`
+
+      this.correction = `$\\dfrac{${result}}{10}=\\dfrac{${miseEnEvidence(num)}}{100}$ (en multipliant par $10$ le numérateur et le dénominateur).`
+
+      this.canEnonce = 'Complète.'
+      this.canReponseACompleter = `$\\dfrac{${result}}{10}=\\dfrac{\\ldots}{100}$`
+    } else {
+      // Cas 4 : result/10 = num/...
+      this.reponse = {
+        champ1: { value: 100 },
+      }
+
+      this.question = `\\dfrac{${result}}{10}=\\dfrac{${num}}{%{champ1}}`
+
+      this.correction = `$\\dfrac{${result}}{10}=\\dfrac{${num}}{${miseEnEvidence(100)}}$ (en multipliant par $10$ le numérateur et le dénominateur).`
+
+      this.canEnonce = 'Complète.'
+      this.canReponseACompleter = `$\\dfrac{${result}}{10}=\\dfrac{${num}}{\\ldots}$`
     }
   }
 
   nouvelleVersion() {
-   
     this.canOfficielle ? this.enonce(10, 1) : this.enonce()
   }
 }
