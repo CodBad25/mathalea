ff5bb574f922180ea6ea80e5dc4524910a07851a
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2026-03-07 04:39:39 +0000

    Jc bosse sur la can

--- src/exercices/can/can6a-2026/can6a-2026-Q15.ts
+++ src/exercices/can/can6a-2026/can6a-2026-Q15.ts
@@ -1,8 +1,8 @@
 import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 import { choice } from '../../../lib/outils/arrayOutils'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
-import { texNombre } from '../../../lib/outils/texNombre'
-import { randint } from '../../../modules/outils'
+import type { IFractionEtendue } from '../../../modules/FractionEtendue.type'
+import { fraction } from '../../../modules/fractions'
 import ExerciceCan from '../../ExerciceCan'
 
 export const titre = 'Q15'
@@ -19,41 +19,31 @@ export const refs = {
 
 */
 export default class Can20266Q15 extends ExerciceCan {
-  enonce(a?: number, b?: number) {
-    let millier: number, centaines: number
-
+  enonce(a?: IFractionEtendue, b?: IFractionEtendue) {
+    let facteur = 2
     if (a == null || b == null) {
-      const m = randint(1, 9)
-      const c = randint(1, 6)
-      const d = randint(1, 9)
-      const u = randint(1, 9)
-      const centainesAjoutees = choice(
-        [3, 4, 6, 7, 8, 9].filter((x) => x + c < 10),
-      )
-      millier = m * 1000 + c * 100 + d * 10 + u
-      centaines = centainesAjoutees * 100
-    } else {
-      millier = a
-      centaines = b
+      const denB = choice([2, 3, 4, 5])
+      const denA = denB * choice([2, 3, 4])
+      const numA = choice([1, 3, 5, 7], denB)
+      const numB = numA
+      a = fraction(numA, denA)
+      b = fraction(numB, denB)
+      facteur = denA / denB
     }
 
-    const somme = millier + centaines
+    this.reponse = b.diviseFraction(a).texFractionSimplifiee
+    this.question = `Combien de fois $${a.texFraction}$ dans $${b.texFraction}$ ?`
 
-    this.reponse = String(somme)
-    if (this.interactif) {
-      this.question = `$${texNombre(millier)}+${centaines}=$`
-    } else {
-      this.question = `$${texNombre(millier)}+${centaines}=\\ldots$`
-    }
-
-    this.correction = `$${texNombre(millier)}+${centaines}=${miseEnEvidence(texNombre(somme))}$`
+    this.correction = `$${b.texFraction}=${b.reduire(facteur).texFraction}$ donc il y a $${miseEnEvidence(this.reponse)}$ fois $${a.texFraction}$ dans $${b.texFraction}$.`
 
     this.formatChampTexte = KeyboardType.clavierDeBase
-    this.canEnonce = 'Calcule.'
-    this.canReponseACompleter = `$${texNombre(millier)}+${centaines}=\\ldots$`
+    this.canEnonce = ''
+    this.canReponseACompleter = ''
   }
 
   nouvelleVersion() {
-    this.canOfficielle ? this.enonce(1462, 300) : this.enonce()
+    this.canOfficielle || this.sup
+      ? this.enonce(fraction(1, 4), fraction(1, 2))
+      : this.enonce()
   }
 }
