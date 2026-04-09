b2f955542acf9d7e8bd0c5221884ffee285c7a32
gillesMora <gilles.mora@ac-normandie.fr>
2026-04-09 18:31:47 +0200

    fix: can3L08

--- src/exercices/can/3e/can3L08.ts
+++ src/exercices/can/3e/can3L08.ts
@@ -1,13 +1,12 @@
 import { point } from '../../../lib/2d/PointAbstrait'
 import { polygone } from '../../../lib/2d/polygones'
 import { segment } from '../../../lib/2d/segmentsVecteurs'
-import { labelPoint, texteParPosition } from '../../../lib/2d/textes'
+import { labelPoint, latex2d } from '../../../lib/2d/textes'
 import { milieu } from '../../../lib/2d/utilitairesPoint'
 import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 import { choice } from '../../../lib/outils/arrayOutils'
 import { reduireAxPlusB, rienSi1 } from '../../../lib/outils/ecritures'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
-import { texNombre } from '../../../lib/outils/texNombre'
 import { mathalea2d } from '../../../modules/mathalea2d'
 import { randint } from '../../../modules/outils'
 import ExerciceSimple from '../../ExerciceSimple'
@@ -31,106 +30,90 @@ export default class EnFonctionDeAire extends ExerciceSimple {
     this.typeExercice = 'simple'
     this.nbQuestions = 1
     this.formatChampTexte = KeyboardType.clavierDeBaseAvecVariable
+     this.optionsChampTexte = { texteApres: '$\\text{ cm}^2$.' }
   }
 
   nouvelleVersion() {
-    switch (choice([1, 2])) {
-      case 1: //
-        {
-          const a = randint(1, 3)
-          const b1 = randint(1, 5)
-          const b = 2 * b1
-          const c = a + b
-          const A = point(0, 0, 'A', 'below')
-          const B = point(5, 0, 'B', 'below')
-          const C = point(5, 3, 'C', 'above')
-          const D = point(0, 3, 'D', 'above')
-          const M = point(2, 3, 'M', 'above')
-          const poly = polygone([A, B, C, M], 'black')
-          const segmentMD = segment(M, D)
-          const segmentDA = segment(D, A)
-          poly.hachures = true
-          // poly.couleurDeRemplissage = colorToLatexOrHTML('lightgray')
-          const d = texteParPosition(
-            `$${c}\\text{ cm}$`,
-            milieu(A, B).x,
-            milieu(A, B).y - 0.5,
-            0,
-            'red',
-            1,
-            'milieu',
-            false,
-          )
-          const e = texteParPosition(
-            `$${texNombre(b, 0)}\\text{ cm}$`,
-            milieu(B, C).x + 1,
-            milieu(B, C).y,
-            0,
-            'black',
-            1,
-            'milieu',
-            false,
-          )
-          const t = texteParPosition(
-            '$x$',
-            milieu(D, M).x,
-            milieu(D, M).y + 0.5,
-            0,
-            'black',
-            1,
-            'milieu',
-            false,
-          )
-          this.question = '$ABCD$ est un rectangle et $DM=x$.<br>'
-          this.question += mathalea2d(
-            {
-              xmin: -1.5,
-              ymin: -1,
-              xmax: 7.1,
-              ymax: 4,
-              pixelsParCm: 30,
-              scale: 0.7,
-            },
-            poly,
-            labelPoint(A, B, C, D, M),
-            d,
-            e,
-            t,
-            segmentMD,
-            segmentDA,
-          )
-          if (choice([true, false])) {
-            this.reponse = {
-              reponse: {
-                value: reduireAxPlusB(b1, 0),
-                options: { fonction: true, variable: 'x' },
-              },
-            }
-            this.question +=
-              "L'aire de la partie non hachurée en fonction de $x$ est : "
-            this.correction = `L'aire du triangle $ADM$ est $\\dfrac{DM\\times AD}{2}=\\dfrac{x\\times ${b}}{2}=${miseEnEvidence(reduireAxPlusB(b1, 0))}\\text{ cm}^2$.`
-          } else {
-            this.reponse = {
-              reponse: {
-                value: reduireAxPlusB(-b1, c * b),
-                options: { fonction: true, variable: 'x' },
-              },
-            }
-            this.question +=
-              "L'aire de la partie  hachurée en fonction de $x$ est : "
-            this.correction = `$\\bullet$ L'aire du triangle $ADM$ est $\\dfrac{DM\\times AD}{2}=\\dfrac{x\\times ${b}}{2}=${reduireAxPlusB(b1, 0)}\\text{ cm}^2$.<br>
-          $\\bullet$ L'aire du rectangle $ABCD$ est $AB\\times BC=${c}\\times ${b} =${c * b}\\text{ cm}^2$.<br>
-          $\\bullet$ L'aire de la partie hachurée est donc la différence entre ces deux aires, soit  $${miseEnEvidence(`${c * b}-${rienSi1(b1)}x`)}\\text{ cm}^2$.`
-          }
+    switch (choice([2,2])) {
+      case 1:
+  {
+    const a = randint(1, 3)
+    const b1 = randint(1, 5)
+    const b = 2 * b1
+    const c = a + b
+    const A = point(0, 0, 'A', 'below')
+    const B = point(5, 0, 'B', 'below')
+    const C = point(5, 3, 'C', 'above')
+    const D = point(0, 3, 'D', 'above')
+    const M = point(2, 3, 'M', 'above')
+    const poly = polygone([A, B, C, M], 'black')
+    const segmentMD = segment(M, D)
+    const segmentDA = segment(D, A)
+    poly.hachures = 'north east lines'
+    const d = latex2d(
+      `${c}\\text{ cm}`,
+      milieu(A, B).x,
+      milieu(A, B).y - 0.5,
+      { letterSize: 'normalsize' },
+    )
+    const e = latex2d(
+      `${b}\\text{ cm}`,
+      milieu(B, C).x + 1,
+      milieu(B, C).y,
+      { letterSize: 'normalsize' },
+    )
+    const t = latex2d('x', milieu(D, M).x, milieu(D, M).y + 0.5, {
+      letterSize: 'normalsize',
+    })
 
-          this.canReponseACompleter = '$\\ldots\\text{ cm}^2$'
-          if (this.interactif) {
-            this.optionsChampTexte = { texteApres: '$\\text{ cm}^2$.' }
-          } else {
-            this.question += ' $\\ldots\\text{ cm}^2$.'
-          }
-        }
-        break
+    const figure = mathalea2d(
+      {
+        xmin: -2,
+        ymin: -1,
+        xmax: 7.1,
+        ymax: 4,
+        pixelsParCm: 30,
+        scale: 0.7,
+      },
+      poly,
+      labelPoint(A, B, C, D, M),
+      d,
+      e,
+      t,
+      segmentMD,
+      segmentDA,
+    )
+    const intro = '$ABCD$ est un rectangle et $DM=x$.<br>' + figure
+
+    if (choice([true, false])) {
+      this.reponse = {
+        reponse: {
+          value: reduireAxPlusB(b1, 0),
+          options: { fonction: true, variable: 'x' },
+        },
+      }
+      const consigne = "L'aire de la partie non hachurée en fonction de $x$ est : "
+      this.question = intro + consigne
+      if (!this.interactif) this.question += ' $\\ldots \\text{ cm}^2$.'
+      this.canEnonce = intro + consigne
+      this.correction = `L'aire du triangle $ADM$ est $\\dfrac{DM\\times AD}{2}=\\dfrac{x\\times ${b}}{2}=${miseEnEvidence(reduireAxPlusB(b1, 0))}\\text{ cm}^2$.`
+    } else {
+      this.reponse = {
+        reponse: {
+          value: reduireAxPlusB(-b1, c * b),
+          options: { fonction: true, variable: 'x' },
+        },
+      }
+      const consigne = "L'aire de la partie  hachurée en fonction de $x$ est : "
+      this.question = intro + consigne
+      if (!this.interactif) this.question += ' $\\ldots \\text{ cm}^2$.'
+      this.canEnonce = intro + consigne
+      this.correction = `$\\bullet$ L'aire du triangle $ADM$ est $\\dfrac{DM\\times AD}{2}=\\dfrac{x\\times ${b}}{2}=${reduireAxPlusB(b1, 0)}\\text{ cm}^2$.<br>
+      $\\bullet$ L'aire du rectangle $ABCD$ est $AB\\times BC=${c}\\times ${b} =${c * b}\\text{ cm}^2$.<br>
+      $\\bullet$ L'aire de la partie hachurée est donc la différence entre ces deux aires, soit  $${miseEnEvidence(`${c * b}-${rienSi1(b1)}x`)}\\text{ cm}^2$.`
+    }
+  }
+  break
 
       case 2:
         {
@@ -149,90 +132,73 @@ export default class EnFonctionDeAire extends ExerciceSimple {
           const poly = polygone([E, M, N, D], 'black')
           const poly1 = polygone([A, E, F, G], 'black')
           const poly2 = polygone([E, B, C, D], 'black')
-          poly.hachures = true
-          poly1.hachures = true
-          const f = texteParPosition(
-            `$${texNombre(a, 0)}\\text{ cm}$`,
-            -1.8,
-            milieu(A, G).y,
-            0,
-            'black',
-            1,
-            'milieu',
-            false,
-          )
-          const e = texteParPosition(
-            `$${texNombre(b, 0)}\\text{ cm}$`,
-            milieu(B, C).x + 1,
+          poly.hachures = 'north east lines'
+          poly1.hachures = 'north east lines'
+          const f = latex2d(`${a}\\text{ cm}`, -1.6, milieu(A, G).y, {
+            letterSize: 'normalsize',
+          })
+          const e = latex2d(
+            `${b}\\text{ cm}`,
+            milieu(B, C).x + 0.8,
             milieu(B, C).y,
-            0,
-            'black',
-            1,
-            'milieu',
-            false,
+            { letterSize: 'normalsize' },
           )
-          const t = texteParPosition(
-            '$x$',
-            milieu(E, M).x,
-            milieu(E, M).y - 0.6,
-            0,
-            'black',
-            1,
-            'milieu',
-            false,
-          )
-          this.question = '$AEFG$ et $EBCD$ sont des carrés et $EM=x$.<br>'
-          this.question += mathalea2d(
-            {
-              xmin: -2.5,
-              ymin: -1,
-              xmax: 7.1,
-              ymax: 6,
-              pixelsParCm: 30,
-              scale: 0.7,
-            },
-            poly,
-            poly1,
-            poly2,
-            labelPoint(A, B, C, D, M, N, E, F, G),
-            e,
-            t,
-            f,
-          )
-          if (choice([true, false])) {
-            this.reponse = {
-              reponse: {
-                value: reduireAxPlusB(-b, b ** 2),
-                options: { fonction: true, variable: 'x' },
-              },
-            }
-            this.question +=
-              "L'aire de la partie non hachurée en fonction de $x$ est : "
-            this.correction = `$\\bullet$ L'aire du rectangle $EMND$ est $EM\\times MN=x \\times ${b} =${b}x\\text{ cm}^2$.<br>
-            $\\bullet$ L'aire du carré $EBCD$ est $${b}^2 =${b ** 2}\\text{ cm}^2$.<br>
-            $\\bullet$ L'aire de la partie non hachurée est donc la différence entre ces deux aires, soit  $${miseEnEvidence(`${b ** 2}-${rienSi1(b)}x`)}\\text{ cm}^2$.`
-          } else {
-            this.reponse = {
-              reponse: {
-                value: reduireAxPlusB(b, a ** 2),
-                options: { fonction: true, variable: 'x' },
-              },
-            }
-            this.question +=
-              "L'aire de la partie  hachurée en fonction de $x$ est : "
-            this.correction = `$\\bullet$ L'aire du carré $AEFG$ est $${a}^2=${a ** 2}\\text{ cm}^2$.<br>
-          $\\bullet$ L'aire du rectangle $EMND$ est $EM\\times MN=x \\times ${b} =${b}x\\text{ cm}^2$.<br>
-          $\\bullet$ L'aire de la partie hachurée est donc la somme de ces deux aires, soit $${miseEnEvidence(reduireAxPlusB(b, a ** 2))}\\text{ cm}^2$.`
-          }
+          const t = latex2d('x', milieu(E, M).x, milieu(E, M).y - 0.6, {
+            letterSize: 'normalsize',
+          })
+         const figure = mathalea2d(
+      {
+        xmin: -3,
+        ymin: -1,
+        xmax: 7.1,
+        ymax: 6,
+        pixelsParCm: 30,
+        scale: 0.7,
+      },
+      poly,
+      poly1,
+      poly2,
+      labelPoint(A, B, C, D, M, N, E, F, G),
+      e,
+      t,
+      f,
+    )
+    const intro = '$AEFG$ et $EBCD$ sont des carrés et $EM=x$.<br>' + figure
 
-          this.canReponseACompleter = '$\\ldots\\text{ cm}^2$'
-          if (this.interactif) {
-            this.optionsChampTexte = { texteApres: '$\\text{ cm}^2$.' }
-          } else {
-            this.question += ' $\\ldots \\text{ cm}^2$.'
-          }
-        }
-        break
+    if (choice([true, false])) {
+      this.reponse = {
+        reponse: {
+          value: reduireAxPlusB(-b, b ** 2),
+          options: { fonction: true, variable: 'x' },
+        },
+      }
+      const consigne = "L'aire de la partie non hachurée en fonction de $x$ est : "
+      this.question = intro + consigne
+      if (!this.interactif) this.question += ' $\\ldots \\text{ cm}^2$.'
+      this.canEnonce = intro + consigne
+      this.correction = `$\\bullet$ L'aire du rectangle $EMND$ est $EM\\times MN=x \\times ${b} =${b}x\\text{ cm}^2$.<br>
+        $\\bullet$ L'aire du carré $EBCD$ est $${b}^2 =${b ** 2}\\text{ cm}^2$.<br>
+        $\\bullet$ L'aire de la partie non hachurée est donc la différence entre ces deux aires, soit  $${miseEnEvidence(`${b ** 2}-${rienSi1(b)}x`)}\\text{ cm}^2$.`
+    } else {
+      this.reponse = {
+        reponse: {
+          value: reduireAxPlusB(b, a ** 2),
+          options: { fonction: true, variable: 'x' },
+        },
+      }
+      const consigne = "L'aire de la partie  hachurée en fonction de $x$ est : "
+      this.question = intro + consigne
+      if (!this.interactif) this.question += ' $\\ldots \\text{ cm}^2$.'
+      this.canEnonce = intro + consigne
+      this.correction = `$\\bullet$ L'aire du carré $AEFG$ est $${a}^2=${a ** 2}\\text{ cm}^2$.<br>
+        $\\bullet$ L'aire du rectangle $EMND$ est $EM\\times MN=x \\times ${b} =${b}x\\text{ cm}^2$.<br>
+        $\\bullet$ L'aire de la partie hachurée est donc la somme de ces deux aires, soit $${miseEnEvidence(reduireAxPlusB(b, a ** 2))}\\text{ cm}^2$.`
     }
+  }
+  break
+    }
+  
+    this.canReponseACompleter = '$\\ldots\\text{ cm}^2$'
+    
   }
 }
