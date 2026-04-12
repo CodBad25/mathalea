8e3d3abb9ccaf879109bb5d3da99a2d8f8615ed3
gillesMora <gilles.mora@ac-normandie.fr>
2026-04-12 11:31:51 +0200

    feat :5S21-1 multimathfield

--- src/exercices/5e/5S21-1_old.ts
+++ src/exercices/5e/5S21-1_old.ts
@@ -1,4 +1,5 @@
 import { handleAnswers } from '../../lib/interactif/gestionInteractif'
+import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive'
 import { choice } from '../../lib/outils/arrayOutils'
 import { numAlpha, sp } from '../../lib/outils/outilString'
 import { texNombre } from '../../lib/outils/texNombre'
@@ -7,21 +8,19 @@ import Exercice from '../Exercice'
 
 import { KeyboardType } from '../../lib/interactif/claviers/keyboard'
 import { miseEnEvidence } from '../../lib/outils/embellissements'
-import { toutAUnPoint } from '../../lib/interactif/mathLive'
-import { addMultiMathfield } from '../../lib/interactif/MultiMathfield/MultiMathfield'
 export const titre = 'Trouver des probabilités simples'
 export const dateDePublication = '01/05/2021'
 export const interactifReady = true
-export const interactifType = 'multiMathfield'
+export const interactifType = 'mathLive'
 /**
  *
  * @author Rémi Angot et Matthieu Devillers
  */
-export const uuid = 'a813f'
+export const uuid = '850b0'
 
 export const refs = {
-  'fr-fr': ['5S21-1', 'BP2FLUC14'],
-  'fr-ch': ['11NO2-7'],
+  'fr-fr': [],
+  'fr-ch': [],
 }
 export default class ProbabilitesSimples extends Exercice {
   constructor() {
@@ -51,35 +50,27 @@ export default class ProbabilitesSimples extends Exercice {
       ])
       texte = `Lors d'un match de ${sport}, l'équipe qui reçoit un adversaire a une probabilité de $ ${texNombre(pG / 100)}$ de gagner son match`
       texte += ` et $${texNombre(pN / 100)}$ de faire un match nul.<br><br>`
- const reponse1 = texNombre((pG + pN) / 100)
- const reponse2 = texNombre(1 - (pG + pN) / 100)
 
-  texte += `${addMultiMathfield(this, i, {
-        dataTemplate: `a) Quelle est la probabilité, pour cette équipe, de ne pas perdre le match ? %{champ1}\nb) Quelle est la probabilité, pour cette équipe, de perdre le match ? %{champ2}`,
-        dataOptions: {
-          champ1: { keyboard: KeyboardType.clavierNumbers },
-          champ2: { keyboard: KeyboardType.clavierNumbers },
-        },
-      })}`
-     
+      texte +=
+        `${numAlpha(0)}Quelle est la probabilité, pour cette équipe, de ne pas perdre le match ?` +
+        ajouteChampTexteMathLive(this, 2 * i, KeyboardType.clavierNumbers)
       let correction1 = `${numAlpha(0)} Ne pas perdre un match, c'est, soit le gagner, soit faire un match nul. La probabilité est donc : <br> <br>`
       correction1 += `P(«${sp(1)}Ne pas perdre le match${sp(1)}») $=$ P(«${sp(1)}Gagner le match${sp(1)}») + P(«${sp(1)}Match nul${sp(1)}») <br>`
       correction1 += `P(«${sp(1)}Ne pas perdre le match${sp(1)}») $= ${texNombre(pG / 100)} + ${texNombre(pN / 100)}$ <br> `
-     
+      const reponse1 = texNombre((pG + pN) / 100)
       correction1 += `P(«${sp(1)}Ne pas perdre le match${sp(1)}») $= ${miseEnEvidence(`${reponse1}`)} $  <br>`
-    
+      texte +=
+        `<br><br>${numAlpha(1)}Quelle est la probabilité, pour cette équipe, de perdre le match ?` +
+        ajouteChampTexteMathLive(this, 2 * i + 1, KeyboardType.clavierNumbers)
       correction1 += `<br><br>${numAlpha(1)} L'évènement  «${sp(1)}Perdre le match${sp(1)}» est l'évènement contraire de  «${sp(1)}Ne pas perdre le match${sp(1)}». On peut donc affirmer que : <br> <br>`
       correction1 += `P(«${sp(1)}Perdre le match${sp(1)}») $+$ P(«${sp(1)}Ne pas perdre le match${sp(1)}») $= 1$ <br>`
       correction1 += `P(«${sp(1)}Perdre le match${sp(1)}») $=1-$ P(«${sp(1)}Ne pas perdre le match${sp(1)}»)<br>`
       correction1 += `P(«${sp(1)}Perdre le match${sp(1)}») $=1-${texNombre((pG + pN) / 100)}$<br>`
-      
+      const reponse2 = texNombre(1 - (pG + pN) / 100)
       correction1 += `P(«${sp(1)}Perdre le match${sp(1)}») $=${miseEnEvidence(`${reponse2}`)} $<br>`
       if (this.questionJamaisPosee(i, pG)) {
-        handleAnswers(this, i, {
-          champ1: { value: reponse1 },
-          champ2: { value: reponse2 },
-          bareme: toutAUnPoint,
-        }, { formatInteractif: 'multiMathfield' })
+        handleAnswers(this, 2 * i, { reponse: { value: reponse1 } })
+        handleAnswers(this, 2 * i + 1, { reponse: { value: reponse2 } })
         this.listeQuestions[i] = texte
         this.listeCorrections[i] = correction1
         i++
