e5d849ce36de8975c61c027a3166088a9ddddf51
Eric Elter <eric.elter@ac-normandie.fr>
2026-04-13 09:39:06 +0200

    5A12-3 passe en multiMathField et devient AMC #9176

--- src/exercices/5e/5A12-3Old.ts
+++ src/exercices/5e/5A12-3Old.ts
@@ -1,4 +1,4 @@
-import ExerciceLabyrinthePremiers3e from '../3e/3A10-7'
+import ExerciceLabyrinthePremiers3eOld from '../3e/3A10-7Old'
 export const titre = 'Parcourir un labyrinthe de nombres premiers'
 export const interactifReady = true
 export const interactifType = 'multiMathField'
@@ -6,18 +6,17 @@ export const amcReady = true
 export const amcType = 'AMCOpen'
 export const dateDePublication = '12/10/2022'
 export const dateDeModifImportante = '29/10/2024'
-export const uuid = '0507e'
+export const uuid = '05079'
 export const refs = {
-  'fr-fr': ['5A12-3'],
-  'fr-ch': ['NR'],
+  'fr-fr': [],
+  'fr-ch': [],
 }
 
 /** Explorer un labyrinthe de nombres premiers
  * @author Éric Elter // Sur la base d'autres labyrinthes déjà créés
- * Passage en multiMahField et AMC par Éric Elter (13/04/2026)
  */
 
-export default class ExerciceLabyrinthePremiers5e extends ExerciceLabyrinthePremiers3e {
+export default class ExerciceLabyrinthePremiers5eOld extends ExerciceLabyrinthePremiers3eOld {
   constructor() {
     super()
     this.sup = 1
