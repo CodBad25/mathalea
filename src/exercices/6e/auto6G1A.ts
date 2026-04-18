b1956f079a3aae40d3a1e4bededa3cc35415e820
Eric Elter <eric.elter@ac-normandie.fr>
2026-04-18 08:22:25 +0200

    auto6G1A : blue reprend la place de bleuMathalea

--- src/exercices/6e/auto6G1A.ts
+++ src/exercices/6e/auto6G1A.ts
@@ -21,7 +21,6 @@ import { context } from '../../modules/context'
 import { mathalea2d } from '../../modules/mathalea2d'
 import { listeQuestionsToContenu, randint } from '../../modules/outils'
 import Exercice from '../Exercice'
-import { bleuMathalea } from '../../lib/colors'
 
 export const amcReady = true
 export const amcType = 'AMCHybride'
@@ -84,7 +83,7 @@ export default class constructionElementaire extends Exercice {
         lettreDepuisChiffre(indLettre + 1),
         'above right',
       )
-      const d = droite(A, B, '', bleuMathalea)
+      const d = droite(A, B, '', 'blue') // Ne pas mettre bleuMathalea
       const C = point(
         randint(2, 4, [A.x]),
         randint(3, 6, [A.y]),
@@ -152,7 +151,7 @@ export default class constructionElementaire extends Exercice {
       const Ymax = Math.ceil(Math.max(A.y, B.y, C.y, D.y, E.y, F.y) + 1)
       anim.recadre(Xmin - 3, Ymax)
       anim.pointsCreer(A, B, C, D)
-      anim.regleDroite(A, B, { couleur: bleuMathalea })
+      anim.regleDroite(A, B, { couleur: 'blue' }) // Ne pas mettre bleuMathalea
       anim.regleSegment(A, C, { couleur: 'red' })
       anim.regleDemiDroiteOriginePoint(C, D, { couleur: 'green' })
       anim.regleMasquer({})
@@ -381,7 +380,7 @@ export default class constructionElementaire extends Exercice {
     const { isValid, message } = checkLineFromLabels({
       figure,
       nameLine: [`(${this.Anom}${this.Bnom})`, `(${this.Bnom}${this.Anom})`],
-      color: bleuMathalea,
+      color: 'blue', // Ne pas mettre bleuMathalea
     })
     resultat.push(isValid ? 'OK' : 'KO')
     if (message !== '') {
