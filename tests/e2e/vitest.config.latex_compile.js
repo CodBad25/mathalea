79672cab640b6703d3e4665501fc53e5173bb51a
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2026-05-30 10:22:59 +0200

    Rectification des chemins

--- tests/e2e/vitest.config.latex_compile.js
+++ tests/e2e/vitest.config.latex_compile.js
@@ -1,7 +1,7 @@
 import { resolve } from 'node:path'
 import { mergeConfig } from 'vite'
 import { defineConfig } from 'vitest/config'
-import viteConfig from '../../vite.config'
+import viteConfig from './vite.config'
 
 export default mergeConfig(
   viteConfig,
