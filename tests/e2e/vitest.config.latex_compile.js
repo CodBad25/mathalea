ed3a506f1c7c600d802e7fcf4fef33d0f905cc9a
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2026-05-30 10:00:50 +0200

    Ajout du fichier vitest.config

---
+++ tests/e2e/vitest.config.latex_compile.js
@@ -0,0 +1,24 @@
+import { resolve } from 'node:path'
+import { mergeConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
+import viteConfig from '../vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    resolve: {
+      alias: {
+        testBrowser: resolve(__dirname, 'e2e'),
+      },
+    },
+    test: {
+      include: ['./tests/latex_compile/*.test.{js,ts}'],
+      environment: 'jsdom',
+      hookTimeout: 120_000,
+      testTimeout: 1_200_000,
+      pool: 'threads',
+      maxWorkers: 1,
+      isolate: false,
+    },
+  }),
+)
