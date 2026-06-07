3537d22e403bb17233d27f90e1a79545fead894a
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2026-06-07 08:49:04 +0000

    Ajout du style classique (Coopmaths) pour la compilation (timeout porté à 2h pour les tests lourds)

--- vitest.config.js
+++ vitest.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['tests/unit/setup-vitest.ts'],
-    testTimeout: 3_600_000,
+    testTimeout: 7_200_000, // prévoir 2h pour un gros (très gros test de compilation)
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
