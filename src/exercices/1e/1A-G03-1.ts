37c0740086e58645ed6295ce6657920d5140ef5c
Stephane Guyon <stephane.guyon@ac-montpellier.fr>
2026-06-19 09:13:47 +0000

    SG Titres automatismes 1ère

--- src/exercices/1e/1A-G03-1.ts
+++ src/exercices/1e/1A-G03-1.ts
@@ -21,7 +21,7 @@ export const interactifReady = true
 export const interactifType = 'qcm'
 export const amcReady = 'true'
 export const amcType = 'qcmMono'
-export const titre = 'Trigonométrie dans le triangle rectangle'
+export const titre = 'Utiliser la trigonométrie dans le triangle rectangle'
 export const dateDePublication = '18/06/2026'
 
 /**
