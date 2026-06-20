fcd55e58d5f2d8d246df2a79a92c0b46b25cd9c2
Stephane Guyon <stephane.guyon@ac-montpellier.fr>
2026-06-20 08:00:11 +0000

    SG 1A-G03-3

--- src/exercices/1e/1A-G02-3.ts
+++ src/exercices/1e/1A-G02-3.ts
@@ -6,17 +6,18 @@ import { labelPoint } from '../../lib/2d/textes'
 import { texteSurSegment } from '../../lib/2d/texteSurSegment'
 import { tracePoint } from '../../lib/2d/TracePoint'
 import { miseEnEvidence } from '../../lib/outils/embellissements'
+import { texNombre } from '../../lib/outils/texNombre'
 import { mathalea2d } from '../../modules/mathalea2d'
 import { randint } from '../../modules/outils'
 import ExerciceQcmA from '../ExerciceQcmA'
 
-export const titre = "Calculer l'aire d'un disque"
+export const titre = "Calculer le périmètre d'un cercle"
 export const dateDePublication = '19/06/2026'
 
-export const uuid = 'f19d3'
+export const uuid = 'c84f2'
 
 export const refs = {
-  'fr-fr': ['1A-G2-04'],
+  'fr-fr': ['1A-G02-3'],
   'fr-ch': [],
 }
 
@@ -26,13 +27,13 @@ export const amcReady = true
 export const amcType = 'qcmMono'
 
 /**
- * Calculer l'aire d'un disque à partir de son diamètre.
+ * Calculer le périmètre d'un cercle à partir de son rayon ou de son diamètre.
  * @author Stéphane Guyon
  */
-export default class AireDisqueQcm extends ExerciceQcmA {
-  private appliquerLesValeurs(diametre: number) {
-    const rayon = diametre / 2
-    const coefficientAire = rayon ** 2
+export default class PerimetreCercleQcm extends ExerciceQcmA {
+  private appliquerLesValeurs(rayon: number, donneLeDiametre: boolean) {
+    const diametre = 2 * rayon
+    const valeurAvecPi314 = 3.14 * diametre
 
     const O = pointAbstrait(0, 0, 'O', 'below')
     const A = pointAbstrait(-2.5, 0)
@@ -40,18 +41,25 @@ export default class AireDisqueQcm extends ExerciceQcmA {
     const cercleC = cercle(O, 2.5)
     cercleC.epaisseur = 2
 
-    const segmentMesure = segment(A, B)
+    const segmentMesure = donneLeDiametre ? segment(A, B) : segment(O, B)
     segmentMesure.epaisseur = 1.5
     segmentMesure.pointilles = 5
+    const mesure = donneLeDiametre ? diametre : rayon
     const objets = [
       cercleC,
       segmentMesure,
       tracePoint(O),
       labelPoint(O),
-      texteSurSegment(`$${diametre}\\text{ cm}$`, A, B, 'black', 0.6),
+      texteSurSegment(
+        `$${mesure}\\text{ cm}$`,
+        donneLeDiametre ? A : O,
+        B,
+        'black',
+        0.6,
+      ),
     ]
 
-    this.enonce = `Le disque ci-dessous n'est pas représenté à l'échelle.<br>
+    this.enonce = `Le cercle ci-dessous n'est pas représenté à l'échelle.<br>
 ${mathalea2d(
   Object.assign(
     { pixelsParCm: 25, scale: 0.8, style: 'margin: auto' },
@@ -59,26 +67,30 @@ ${mathalea2d(
   ),
   objets,
 )}
-Quelle est la valeur exacte de son aire ?`
+Quelle est la valeur exacte de son périmètre ?`
 
     this.reponses = [
-      `$${coefficientAire}\\pi\\text{ cm}^2$`,
+      `$${diametre}\\pi\\text{ cm}$`,
+      donneLeDiametre
+        ? `$\\pi\\times ${diametre}^2\\text{ cm}$`
+        : `$\\pi\\times ${rayon}^2\\text{ cm}$`,
+      `$${texNombre(valeurAvecPi314, 2)}\\text{ cm}$`,
       `$${diametre}\\pi\\text{ cm}^2$`,
-      `$${diametre ** 2}\\pi\\text{ cm}^2$`,
-      `$${2 * coefficientAire}\\pi\\text{ cm}^2$`,
     ]
 
-    this.correction = `Le rayon est la moitié du diamètre : $r=${diametre}\\div 2=${rayon}\\text{ cm}$.<br>
-L'aire d'un disque de rayon $r$ est $\\mathcal{A}=\\pi\\times r^2$.<br>
-Ainsi, $\\mathcal{A}=\\pi\\times ${rayon}^2=${miseEnEvidence(`${coefficientAire}\\pi\\text{ cm}^2`)}$.`
+    this.correction = donneLeDiametre
+      ? `Le périmètre d'un cercle de diamètre $D$ est $P=\\pi\\times D$.<br>
+Ainsi, $P=\\pi\\times ${diametre}=${miseEnEvidence(`${diametre}\\pi\\text{ cm}`)}$.`
+      : `Le périmètre d'un cercle de rayon $r$ est $P=2\\times\\pi\\times r$.<br>
+Ainsi, $P=2\\times\\pi\\times ${rayon}=${miseEnEvidence(`${diametre}\\pi\\text{ cm}`)}$.`
   }
 
   versionOriginale = () => {
-    this.appliquerLesValeurs(8)
+    this.appliquerLesValeurs(4, false)
   }
 
   versionAleatoire = () => {
-    this.appliquerLesValeurs(2 * randint(3, 9))
+    this.appliquerLesValeurs(randint(3, 9), randint(1, 2) === 1)
   }
 
   constructor() {
