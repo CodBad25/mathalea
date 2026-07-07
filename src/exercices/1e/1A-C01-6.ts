05fbe1301c69b6c64d9666ad72279e281bae4545
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2026-07-07 05:41:02 +0000

    Resolve "1A-C01-2 : Pas d'équivalent spécifique 1q3 alors que celui-ci est proche"

---
+++ src/exercices/1e/1A-C01-6.ts
@@ -0,0 +1,125 @@
+import { aLeBonNombreDePropsDifferentes } from '../../lib/interactif/qcm'
+// import ExerciceQcmA from '../../ExerciceQcmA'
+import ce from '../../lib/interactif/comparisonFunctions'
+import { choice } from '../../lib/outils/arrayOutils'
+import { miseEnEvidence } from '../../lib/outils/embellissements'
+import { texNombre } from '../../lib/outils/texNombre'
+import { randint } from '../../modules/outils'
+import ExerciceQcmA from '../ExerciceQcmA'
+
+export const uuid = '21eb5'
+export const refs = {
+  'fr-fr': ['1A-C01-6'],
+  'fr-ch': [],
+}
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const amcReady = 'true'
+export const amcType = 'qcmMono'
+export const titre = 'Trouver le plus grand ou le plus petit nombre (bis)'
+export const dateDePublication = '06/07/2026'
+/**
+ *
+ * @author Jean-Claude Lhote (sans IA)
+ *
+ */
+export default class Auto1AC16 extends ExerciceQcmA {
+  private appliquerLesValeurs(
+    den1: number,
+    exp1: number,
+    dec: number,
+    denExp: number,
+    plusGrand: boolean,
+  ): void {
+    const valeur1 = `(\\frac{1}{${den1}})^{${exp1}}`
+    const valeur2 = `(\\frac{1}{${exp1}})^{${den1}}`
+    const valeur3 = `${texNombre(dec, 5)}`
+    const valeur4 = `(\\frac{1}{${denExp}})^{${denExp}}`
+    const valeurs = [valeur1, valeur2, valeur3, valeur4]
+    const valeursNum = valeurs.map((v) =>
+      parseFloat(ce.parse(v.replace(',', '.')).N().toString()),
+    )
+    const isGood = new Set(valeursNum).size === 4
+    const greater = Math.max(...valeursNum)
+    const smaller = Math.min(...valeursNum)
+    const indexBr = valeursNum.indexOf(plusGrand ? greater : smaller)
+    const bonneReponse = valeurs[indexBr]
+    const otherReponses = valeurs.filter((v) => v !== bonneReponse)
+    this.reponses = [bonneReponse, ...otherReponses].map(
+      (v) => `$${v.replace('\\frac', '\\dfrac')}$`,
+    )
+    this.enonce = `Quel est le ${plusGrand ? 'plus grand' : 'plus petit'} nombre parmi les quatre suivants ?`
+    this.correction = `$${valeur1} = \\dfrac{1}{${den1}^{${exp1}}} = \\dfrac{1}{${texNombre(Math.pow(den1, exp1), 0)}}$<br><br>
+    $${valeur2} = \\dfrac{1}{${exp1}^{${den1}}} = \\dfrac{1}{${texNombre(Math.pow(exp1, den1), 0)}}$<br><br>
+    $${valeur3} = \\dfrac{${texNombre(dec * 100, 0)}}{100}${dec.toFixed(2) === '0.01' ? '' : dec.toFixed(2) === '0.03' ? `=\\dfrac{1}{33,3_{\\dots}}` : `=\\dfrac{1}{${(1 / dec).toFixed(0)}}`}$<br><br>
+$${valeur4} = \\dfrac{1}{${denExp}^{${denExp}}} = \\dfrac{1}{${texNombre(Math.pow(denExp, denExp), 0)}}$<br><br>`
+    const denominateurs = [
+      texNombre(Math.pow(den1, exp1), 0),
+      texNombre(Math.pow(exp1, den1), 0),
+      dec.toFixed(2) === '0.01'
+        ? '100'
+        : dec.toFixed(2) === '0.03'
+          ? `33,3_{\\dots}`
+          : (1 / dec).toFixed(0),
+      texNombre(Math.pow(denExp, denExp), 0),
+    ]
+
+    this.correction += `$${denominateurs
+      .map<[string, number]>((v, i) => [v, valeursNum[i]])
+      .sort((a, b) => (plusGrand ? b[1] - a[1] : a[1] - b[1]))
+      .map((v, i) => `${v[0]}`)
+      .join(`${plusGrand ? '<' : '>'}`)}$<br><br>
+      À numérateur égal, le plus ${plusGrand ? 'grand' : 'petit'} nombre est celui qui a le plus ${plusGrand ? 'petit' : 'grand'} dénominateur.<br><br>`
+    this.correction += `$${[valeur1, valeur2, valeur3, valeur4]
+      .map<[string, number]>((v, i) => [
+        v.replace('\\frac', '\\dfrac'),
+        valeursNum[i],
+      ])
+      .sort((a, b) => (plusGrand ? b[1] - a[1] : a[1] - b[1]))
+      .map((v, i) => (i === 0 ? `${miseEnEvidence(v[0])}` : `${v[0]}`))
+      .join(`${plusGrand ? '>' : '<'}`)}$<br><br>`
+    if (!isGood) this.reponses.pop() // j'enlève une réponse si les 4 valeurs ne sont pas différentes, pour éviter d'avoir 2 réponses identiques
+  }
+
+  versionOriginale: () => void = () => {
+    this.appliquerLesValeurs(5, 2, 0.05, 3, true)
+  }
+
+  versionAleatoire: () => void = () => {
+    let compteur = 0
+    let den1: number
+    let exp1: number
+    let dec: number
+    let denExp: number
+    let plusGrand: boolean
+    do {
+      den1 = choice([2, 3, 5, 10])
+      exp1 = choice([2, 3, 5, 10], den1)
+      dec = parseFloat((randint(1, 4) / 100).toFixed(2))
+      denExp = choice([2, 3, 4], [den1, exp1])
+      plusGrand = choice([true, false])
+      this.appliquerLesValeurs(den1, exp1, dec, denExp, plusGrand)
+      compteur++
+    } while (compteur < 100 && !aLeBonNombreDePropsDifferentes(this, 4, true)) // On s'assure d'avoir 4 réponses différentes, sinon on régénère
+  }
+
+  constructor() {
+    super()
+    this.tip = `
+  <p style="margin: 0 0 10px 0;">
+    Pour comparer des nombres, selon les situations, il est souvent plus pratique de tous les écrire :
+  </p>
+  <ul style="list-style-type: disc; padding-left: 1.5em; margin: 0 0 14px 0; line-height: 2;">
+    <li>sous forme de fractions de même dénominateur,</li>
+    <li>sous forme décimale,</li>
+    <li>en notation scientifique.</li>
+  </ul>
+   <p style="margin: 0 0 10px 0;">
+   Le plus simple dans cet exercice est sans doute de les écrire sous forme fractionnaire de même numérateur.
+  </p>
+ `
+    this.versionAleatoire()
+    this.spacing = 1
+    this.spacingCorr = 1
+  }
+}
