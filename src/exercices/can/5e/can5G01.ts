46ea663c9420485cc928d5c91a3f10e2ca59a393
Eric Elter <eric.elter@ac-normandie.fr>
2026-07-07 23:14:42 +0200

    can5G01 : Changement d'un paramètre (Nx programmes 5e)

--- src/exercices/can/5e/can5G01.ts
+++ src/exercices/can/5e/can5G01.ts
@@ -3,23 +3,22 @@ export const interactifReady = true
 export const interactifType = 'mathLive'
 export const amcReady = true
 export const amcType = 'AMCHybride'
-export const titre = 'Trouver le symétrique d’un point 5e'
+export const titre = 'Trouver le symétrique d’un point'
 
 /**
  * @author Jean-claude Lhote
- * Créé pendant l'été 2021
-
  */
+
 export const uuid = '6314c'
 
 export const refs = {
   'fr-fr': ['can5G01'],
   'fr-ch': [],
 }
-export default class SymetriqueD1Point extends Transformations {
+export default class SymetriqueD1Point5eme extends Transformations {
   constructor() {
     super()
-    this.sup = 2
+    this.sup = '1-2-3-4'
     this.can = true
   }
 }
