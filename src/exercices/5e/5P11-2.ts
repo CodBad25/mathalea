fbb4ff4b20cb98a8f85732b12e403654e43f28b4
Olivier Mimeau <olivier.mimeau@ac-orleans-tours.fr>
2026-07-08 00:00:38 +0000

    Om 5p11 2 correctionalternative

--- src/exercices/5e/5P11-2.ts
+++ src/exercices/5e/5P11-2.ts
@@ -7,12 +7,14 @@ import {
   texteEnCouleurEtGras,
 } from '../../lib/outils/embellissements'
 import { texNombre } from '../../lib/outils/texNombre'
+import { getLang } from '../../lib/stores/languagesStore'
 import Grandeur from '../../modules/Grandeur'
 import { listeQuestionsToContenu } from '../../modules/outils'
 import Exercice from '../Exercice'
 
 export const titre = 'Convertir des vitesses entre m/s et km/h'
 export const dateDePublication = '19/08/2025'
+export const dateModiImportante = '07/07/2026'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const uuid = 'cv001'
@@ -24,6 +26,7 @@ export const refs = {
 /**
  * Exercice de conversion de vitesses entre m/s et km/h utilisant la classe Grandeur
  * @author Nathan Scheinmann
+ * olivier Mimeau : correction adapté sur le referentiel 'fr-fr'
  */
 export default class ConvertirVitesse extends Exercice {
   constructor() {
@@ -37,8 +40,11 @@ export default class ConvertirVitesse extends Exercice {
       '1 : m/s vers km/h\n2 : km/h vers m/s\n3 : Mélange',
     ]
     this.besoinFormulaire2CaseACocher = ['Valeurs plus simples', false]
-    this.correctionDetailleeDisponible = true
-    this.correctionDetaillee = true
+    const lang = getLang()
+    if (lang === 'fr-CH') {
+      this.correctionDetailleeDisponible = true
+      this.correctionDetaillee = true
+    }
   }
 
   nouvelleVersion(): void {
@@ -72,8 +78,8 @@ export default class ConvertirVitesse extends Exercice {
     }
 
     for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50; i++) {
-      let texte: string
-      let texteCorr: string
+      let texte: string = ''
+      let texteCorr: string = ''
       const typeDeQuestion = listeTypeDeQuestions[i]
       let valeur: number
       let uniteDepart: string
@@ -130,59 +136,78 @@ export default class ConvertirVitesse extends Exercice {
             texteApres: ` $\\text{${uniteArrivee}}$`,
           })
       }
-
-      // Préparer la correction détaillée
-      if (this.correctionDetaillee) {
+      const lang = getLang()
+      if (lang === 'fr-FR') {
         if (typeDeQuestion === 1) {
           // m/s → km/h
-          texteCorr = `Pour convertir des vitesses de $\\text{\\,m/s}$ en $\\text{\\,km/h}$, nous devons comprendre la relation entre ces deux unités.<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Étape 1 : Analyser les unités', 'green')}.<br>`
-          texteCorr += ` Dans $1\\text{\\,m/s}$ : on parcourt $1$ mètre en $1$ seconde.<br>`
-          texteCorr += ` Dans $1\\text{\\,km/h}$ : on parcourt $1$ kilomètre en $1$ heure.<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Étape 2 : Établir les équivalences', 'green')}.<br>`
-          texteCorr += ` $1\\text{\\,km} = 1\\,000\\text{\\,m}$<br>`
-          texteCorr += ` $1\\text{\\,h} = 60\\text{\\,min} = 60 \\times 60\\text{\\,s} = 3\\,600\\text{\\,s}$<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Étape 3 : Calculer le facteur de conversion', 'green')}.<br>`
-          texteCorr += `Pour convertir $1\\text{\\,m/s}$ en $\\text{km/h}$ :<br>`
-          texteCorr += `$1\\text{\\,m/s} = \\dfrac{1\\text{\\,m}}{1\\text{\\,s}}$<br><br>`
-          texteCorr += `En convertissant les unités :<br>`
-          texteCorr += `$\\dfrac{1\\text{\\,m}}{1\\text{\\,s}} = \\dfrac{1\\text{\\,m} \\times \\dfrac{1\\text{\\,km}}{1\\,000\\text{\\,m}}}{1\\text{\\,s} \\times \\dfrac{1\\text{\\,h}}{3\\,600\\text{\\,s}}} = \\dfrac{\\dfrac{1}{1\\,000}\\text{\\,km}}{\\dfrac{1}{3\\,600}\\text{\\,h}} = \\dfrac{1}{1\\,000} \\times \\dfrac{3\\,600}{1}\\,\\dfrac{\\text{km}}{\\text{h}} = \\dfrac{3\\,600}{1\\,000}\\,\\dfrac{\\text{\\,km}}{\\text{h}} = 3,6\\text{\\,km/h}$<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Règle : Pour convertir des m/s en km/h, on multiplie par 3,6', 'green')}.<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Étape 4 : Application numérique', 'green')}.<br>`
-          texteCorr += `$${texNombre(valeur, 2)}\\text{\\,m/s} = ${texNombre(valeur, 2)} \\times 3,6 = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,km/h}.$`
+          texteCorr = `Pour convertir des vitesses de $\\text{\\,m/s}$ en $\\text{\\,km/h}$, on utilise le sens de ces unités.<br>`
+          texteCorr += `$${texNombre(valeur, 2)}\\text{\\,m/s}$ signifie qu'en $1 \\text{\\,s}$, on parcourt $${texNombre(valeur, 2)}\\text{\\,m}$.<br>`
+          texteCorr += `En $1 \\text{\\,h} = 3\\,600 \\text{\\,s}$, on parcourt donc $${texNombre(valeur, 2)} \\times 3\\,600 \\text{\\,m} = ${texNombre(valeur * 3600, 2)} \\text{\\,m}$.<br>`
+          texteCorr += `$1\\text{\\,km} = 1\\,000\\text{\\,m}$, donc $${texNombre(valeur * 3600, 2)} \\text{\\,m} = ${texNombre(valeur * 3600, 2)} \\times \\dfrac{1}{1\\,000} \\text{\\,km} = ${texNombre(reponse, 2)}\\text{\\,km}$.<br>`
+          texteCorr += `En $1 \\text{\\,h}$, on parcourt $${texNombre(reponse, 2)} \\text{\\,km}$.<br>`
+          texteCorr += `donc $${texNombre(valeur, 2)}\\text{\\,m/s} = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,km/h}.$`
         } else {
           // km/h → m/s
-          texteCorr = `Pour convertir des vitesses de $\\text{\\,km/h}$ en $\\text{\\,m/s}$, nous devons établir la relation entre ces unités.<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Étape 1 : Analyser les unités', 'green')}.<br>`
-          texteCorr += ` Dans $1\\text{\\,km/h}$ : on parcourt $1$ kilomètre en $1$ heure.<br>`
-          texteCorr += ` Dans $1\\text{\\,m/s}$ : on parcourt $1$ mètre en $1$ seconde.<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Étape 2 : Établir les équivalences', 'green')}.<br>`
-          texteCorr += ` $1\\text{\\,km} = 1\\,000\\text{\\,m}$<br>`
-          texteCorr += ` $1\\text{\\,h} = 60\\text{\\,min} = 60 \\times 60\\text{\\,s} = 3\\,600\\text{\\,s}$<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Étape 3 : Calculer le facteur de conversion', 'green')}.<br>`
-          texteCorr += `Pour convertir $1\\text{\\,km/h}$ en $\\text{\\,m/s}$ :<br>`
-          texteCorr += `$1\\text{\\,km/h} = \\dfrac{1\\text{\\,km}}{1\\text{\\,h}}= \\dfrac{1\\,000\\text{\\,m}}{3\\,600\\text{\\,s}} =  1\\div 3,6\\text{\\,m/s}$<br><br>`
-          texteCorr += `${texteEnCouleurEtGras('Règle : Pour convertir des km/h en m/s, on divise par 3,6', 'green')}.<br><br>`
-
-          texteCorr += `${texteEnCouleurEtGras('Étape 4 : Application numérique', 'green')}.<br>`
-          texteCorr += `$${texNombre(valeur, 2)}\\text{\\,km/h} = ${texNombre(valeur, 2)} \\div 3,6 = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,m/s}.$`
+          texteCorr = `Pour convertir des vitesses de $\\text{\\,km/h}$ en $\\text{\\,m/s}$, on utilise le sens de ces unités.<br>`
+          texteCorr += `$${texNombre(valeur, 2)}\\text{\\,km/h}$ signifie qu'en $1 \\text{\\,h}$, on parcourt $${texNombre(valeur, 2)}\\text{\\,km}$.<br>`
+          texteCorr += `$1\\text{\\,km} = 1\\,000\\text{\\,m}$, donc $${texNombre(valeur, 2)} \\text{\\,km} = ${texNombre(valeur, 2)} \\times 1\\,000 \\text{\\,m} = ${texNombre(valeur * 1000, 2)}\\text{\\,m}$.<br>`
+          texteCorr += `$1 \\text{\\,h} = 3\\,600 \\text{\\,s}$, donc en $1 \\text{\\,s}$ on parcourt donc $${texNombre(valeur * 1000, 2)} \\text{\\,m} \\times \\dfrac{1}{3\\,600}  = ${texNombre(reponse, 2)} \\text{\\,m}$.<br>`
+          texteCorr += `donc $${texNombre(valeur, 2)}\\text{\\,km/h} = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,m/s}.$`
         }
-      } else {
-        // Correction simple
-        if (typeDeQuestion === 1) {
-          texteCorr = `${texteEnCouleurEtGras('Conversion m/s → km/h : multiplier par 3,6', 'green')}.<br><br>`
-          texteCorr += `$${texNombre(valeur, 2)}\\text{\\,m/s} = ${texNombre(valeur, 2)} \\times 3,6 = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,km/h}.$`
+      } else if (lang === 'fr-CH') {
+        // Préparer la correction détaillée
+        if (this.correctionDetaillee) {
+          if (typeDeQuestion === 1) {
+            // m/s → km/h
+            texteCorr = `Pour convertir des vitesses de $\\text{\\,m/s}$ en $\\text{\\,km/h}$, nous devons comprendre la relation entre ces deux unités.<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Étape 1 : Analyser les unités', 'green')}.<br>`
+            texteCorr += ` Dans $1\\text{\\,m/s}$ : on parcourt $1$ mètre en $1$ seconde.<br>`
+            texteCorr += ` Dans $1\\text{\\,km/h}$ : on parcourt $1$ kilomètre en $1$ heure.<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Étape 2 : Établir les équivalences', 'green')}.<br>`
+            texteCorr += ` $1\\text{\\,km} = 1\\,000\\text{\\,m}$<br>`
+            texteCorr += ` $1\\text{\\,h} = 60\\text{\\,min} = 60 \\times 60\\text{\\,s} = 3\\,600\\text{\\,s}$<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Étape 3 : Calculer le facteur de conversion', 'green')}.<br>`
+            texteCorr += `Pour convertir $1\\text{\\,m/s}$ en $\\text{km/h}$ :<br>`
+            texteCorr += `$1\\text{\\,m/s} = \\dfrac{1\\text{\\,m}}{1\\text{\\,s}}$<br><br>`
+            texteCorr += `En convertissant les unités :<br>`
+            texteCorr += `$\\dfrac{1\\text{\\,m}}{1\\text{\\,s}} = \\dfrac{1\\text{\\,m} \\times \\dfrac{1\\text{\\,km}}{1\\,000\\text{\\,m}}}{1\\text{\\,s} \\times \\dfrac{1\\text{\\,h}}{3\\,600\\text{\\,s}}} = \\dfrac{\\dfrac{1}{1\\,000}\\text{\\,km}}{\\dfrac{1}{3\\,600}\\text{\\,h}} = \\dfrac{1}{1\\,000} \\times \\dfrac{3\\,600}{1}\\,\\dfrac{\\text{km}}{\\text{h}} = \\dfrac{3\\,600}{1\\,000}\\,\\dfrac{\\text{\\,km}}{\\text{h}} = 3,6\\text{\\,km/h}$<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Règle : Pour convertir des m/s en km/h, on multiplie par 3,6', 'green')}.<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Étape 4 : Application numérique', 'green')}.<br>`
+            texteCorr += `$${texNombre(valeur, 2)}\\text{\\,m/s} = ${texNombre(valeur, 2)} \\times 3,6 = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,km/h}.$`
+          } else {
+            // km/h → m/s
+            texteCorr = `Pour convertir des vitesses de $\\text{\\,km/h}$ en $\\text{\\,m/s}$, nous devons établir la relation entre ces unités.<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Étape 1 : Analyser les unités', 'green')}.<br>`
+            texteCorr += ` Dans $1\\text{\\,km/h}$ : on parcourt $1$ kilomètre en $1$ heure.<br>`
+            texteCorr += ` Dans $1\\text{\\,m/s}$ : on parcourt $1$ mètre en $1$ seconde.<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Étape 2 : Établir les équivalences', 'green')}.<br>`
+            texteCorr += ` $1\\text{\\,km} = 1\\,000\\text{\\,m}$<br>`
+            texteCorr += ` $1\\text{\\,h} = 60\\text{\\,min} = 60 \\times 60\\text{\\,s} = 3\\,600\\text{\\,s}$<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Étape 3 : Calculer le facteur de conversion', 'green')}.<br>`
+            texteCorr += `Pour convertir $1\\text{\\,km/h}$ en $\\text{\\,m/s}$ :<br>`
+            texteCorr += `$1\\text{\\,km/h} = \\dfrac{1\\text{\\,km}}{1\\text{\\,h}}= \\dfrac{1\\,000\\text{\\,m}}{3\\,600\\text{\\,s}} =  1\\div 3,6\\text{\\,m/s}$<br><br>`
+            texteCorr += `${texteEnCouleurEtGras('Règle : Pour convertir des km/h en m/s, on divise par 3,6', 'green')}.<br><br>`
+
+            texteCorr += `${texteEnCouleurEtGras('Étape 4 : Application numérique', 'green')}.<br>`
+            texteCorr += `$${texNombre(valeur, 2)}\\text{\\,km/h} = ${texNombre(valeur, 2)} \\div 3,6 = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,m/s}.$`
+          }
         } else {
-          texteCorr = `${texteEnCouleurEtGras('Conversion km/h → m/s : diviser par 3,6', 'green')}<br><br>`
-          texteCorr += `$${texNombre(valeur, 2)}\\text{\\,km/h} = ${texNombre(valeur, 2)} \\div 3,6 = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,m/s}.$`
+          // Correction simple
+          if (typeDeQuestion === 1) {
+            texteCorr = `${texteEnCouleurEtGras('Conversion m/s → km/h : multiplier par 3,6', 'green')}.<br><br>`
+            texteCorr += `$${texNombre(valeur, 2)}\\text{\\,m/s} = ${texNombre(valeur, 2)} \\times 3,6 = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,km/h}.$`
+          } else {
+            texteCorr = `${texteEnCouleurEtGras('Conversion km/h → m/s : diviser par 3,6', 'green')}<br><br>`
+            texteCorr += `$${texNombre(valeur, 2)}\\text{\\,km/h} = ${texNombre(valeur, 2)} \\div 3,6 = ${miseEnEvidence(`${texNombre(reponse, 2)}`)}\\text{\\,m/s}.$`
+          }
         }
       }
 
