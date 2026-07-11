59d103d0af97f403715ba42b45cca18340aa9a59
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2026-07-12 00:34:14 +0200

    Remise en place fraction_visualizer

--- src/lib/apps/fraction_visualizer.ts
+++ src/lib/apps/fraction_visualizer.ts
@@ -1,12 +1,29 @@
+import FractionVisualizer from 'fraction-visualizer'
 import { context } from '../../modules/context'
-import FractionVisualizerWrapperElement, {
-  type FractionVisualizerWrapperOptions,
-} from '../customElements/FractionVisualizerWrapperElement'
 
 export type ShapeName = 'square' | 'disk' | 'rectangle' | 'bar' | 'segment'
 export type BorderMode = 'none' | 'filled' | 'all' | 'cursor'
 
-type FractionVisualizerOptions = FractionVisualizerWrapperOptions
+/**
+ * Définit le customElement fraction-visualizer
+ */
+if (customElements.get('fraction-visualizer') === undefined) {
+  customElements.define('fraction-visualizer', FractionVisualizer)
+}
+
+type FractionVisualizerOptions = {
+  shape?: ShapeName
+  filled?: number
+  total?: number
+  showFilledSlider?: boolean
+  borderMode?: BorderMode
+  showPartNumbers?: boolean
+  showLabels?: boolean
+  labelStart?: number
+  labelValues?: string
+  sliderMaxInput?: number
+  borderCount?: number
+}
 
 /**
  * Retourne le code HTML pour afficher un fraction-visualizer interactif
@@ -31,5 +48,30 @@ export function fractionVisualizer(
   if (!context.isHtml) {
     return '' // La sortie LaTeX n'est pas encore gérée
   }
-  return `<div class="block">${FractionVisualizerWrapperElement.create(options)}</div>`
+  let optionsString: string = ''
+  if (options) {
+    if (options.shape !== undefined)
+      optionsString += `shape="${options.shape}" `
+    if (options.filled !== undefined)
+      optionsString += `filled="${options.filled}" `
+    if (options.total !== undefined)
+      optionsString += `total="${options.total}" `
+    if (options.showFilledSlider !== undefined)
+      optionsString += `show-filled-slider="${options.showFilledSlider}" `
+    if (options.borderMode !== undefined)
+      optionsString += `border-mode="${options.borderMode}" `
+    if (options.showPartNumbers !== undefined)
+      optionsString += `show-part-numbers="${options.showPartNumbers}" `
+    if (options.showLabels !== undefined)
+      optionsString += `show-labels="${options.showLabels}" `
+    if (options.labelStart !== undefined)
+      optionsString += `label-start="${options.labelStart}" `
+    if (options.labelValues !== undefined)
+      optionsString += `label-values="${options.labelValues}" `
+    if (options.sliderMaxInput !== undefined)
+      optionsString += `slider-max-input="${options.sliderMaxInput}" `
+    if (options.borderCount !== undefined)
+      optionsString += `border-count="${options.borderCount}" `
+  }
+  return `<div class="block"><fraction-visualizer ${optionsString}></fraction-visualizer></div>`
 }
