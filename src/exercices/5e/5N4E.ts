661554eb3ac75107faea069cd1a91186b99506bb
Eric Elter <eric.elter@ac-normandie.fr>
2026-09-02 20:20:16 +0200

    New 5N4E (Nouveau programme de 5ème)

---
+++ src/exercices/5e/5N4E.ts
@@ -0,0 +1,28 @@
+import PrioritesEtRelatifsEtPuissances from '../4e/4C34'
+
+export const dateDePublication = '02/09/2026'
+export const interactifReady = true
+
+export const amcReady = true
+export const amcType = 'AMCHybride'
+
+export const titre =
+  'Calculer en utilisant les priorités opératoires et les puissances'
+
+export const uuid = '24a54'
+
+export const refs = {
+  'fr-fr': ['5N4E'],
+  'fr-ch': [],
+}
+/**
+ * @author Éric Elter
+ */
+
+export default class PrioritesEtRelatifsEtPuissances5e extends PrioritesEtRelatifsEtPuissances {
+  constructor() {
+    super()
+    this.sup2 = '3'
+    this.sup3 = true
+  }
+}
