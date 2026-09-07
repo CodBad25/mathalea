ec927a33691186d8908951e3f313728d6c8638a1
Rémi Angot <remi.angot@gmail.com>
2026-09-07 16:33:47 +0200

    fix: 3L13-1 affichage du signe quand d est négatif dans la correction
    Les branches ax+b=cx+d et k-(ax+b)=cx+d écrivaient le membre de
    droite avec =${c}x+${d}, ce qui produisait un affichage fautif du
    type -9x+-1 quand d<0. Remplacement de +${d} par ecritureAlgebrique(d).

--- src/exercices/3e/3L13-1.ts
+++ src/exercices/3e/3L13-1.ts
@@ -170,7 +170,7 @@ export default class ExerciceEquation1Tiret2 extends Exercice {
               texteCorr += `On ajoute $${rienSi1(-1 * c)}x$ aux deux membres.<br>`
             }
           }
-          texteCorr += `$${rienSi1(a)}x${ecritureAlgebrique(b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x', bleuMathalea)}=${c}x+${d}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x', bleuMathalea)}$<br>`
+          texteCorr += `$${rienSi1(a)}x${ecritureAlgebrique(b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x', bleuMathalea)}=${c}x${ecritureAlgebrique(d)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x', bleuMathalea)}$<br>`
           texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}=${d}$<br>`
           if (this.correctionDetaillee) {
             if (b > 0) {
@@ -251,7 +251,7 @@ export default class ExerciceEquation1Tiret2 extends Exercice {
               texteCorr += `On ajoute $${rienSi1(-1 * c)}x$ aux deux membres.<br>`
             }
           }
-          texteCorr += `$${rienSi1(a)}x${ecritureAlgebrique(b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x', bleuMathalea)}=${c}x+${d}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x', bleuMathalea)}$<br>`
+          texteCorr += `$${rienSi1(a)}x${ecritureAlgebrique(b)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x', bleuMathalea)}=${c}x${ecritureAlgebrique(d)}${miseEnEvidence(signe(-1 * c) + rienSi1(abs(c)) + 'x', bleuMathalea)}$<br>`
           texteCorr += `$${rienSi1(a - c)}x${ecritureAlgebrique(b)}=${d}$<br>`
           if (this.correctionDetaillee) {
             if (b > 0) {
