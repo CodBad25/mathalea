a1525826054149b4159543c551acec70124d5d67
Eric Elter <eric.elter@ac-normandie.fr>
2026-09-08 21:46:24 +0200

    3A11-0Old : Suppression des ref

--- src/exercices/3e/3A11-0Old.ts
+++ src/exercices/3e/3A11-0Old.ts
@@ -8,8 +8,8 @@ export const dateDePublication = '28/09/2022'
 export const dateDeModifImportante = '03/10/2023'
 export const uuid = '1871d'
 export const refs = {
-  'fr-fr': ['3A11-0'],
-  'fr-ch': ['9NO3B-5'],
+  'fr-fr': [],
+  'fr-ch': [],
 }
 export default class SimplifierFractions3eOld extends SimplifierFractionsOld {
   constructor() {
