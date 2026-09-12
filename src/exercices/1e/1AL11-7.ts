360c1fb2b706b540379c3f78142a14a9d5bbcee4
Remi Angot <remi.angot@ac-montpellier.fr>
2026-09-12 08:03:31 +0000

    fix: corriger l'exposant ^() en ^{} pour la validation interactive des suites géométriques

--- src/exercices/1e/1AL11-7.ts
+++ src/exercices/1e/1AL11-7.ts
@@ -105,7 +105,7 @@ export default class TermeGeneralSuiteParRecurrence extends Exercice {
         ])
         const raisonTex = texNombre(raison, 1)
         const puissance = exposant(indicePremierTerme)
-        reponse = `${premierTerme}*(${raison.toString()})^(${puissance})`
+        reponse = `${premierTerme}*(${raison.toString()})^{${puissance}}`
         relation = `u_{n+1}=${raisonTex}u_n`
         texteCorr = `$u_{n+1}=${raisonTex}u_n$ est la relation de récurrence d’une suite géométrique de raison $q=${raisonTex}$.<br>`
         texteCorr +=
