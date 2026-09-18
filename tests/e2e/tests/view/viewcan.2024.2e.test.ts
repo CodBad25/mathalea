05e73cc0b6ecfae1d727a57bcc050f16121d3fdc
Rémi Angot <remi.angot@gmail.com>
2026-09-18 10:40:58 +0200

    fix: mise à jour des tests

--- tests/e2e/tests/view/viewcan.2024.2e.test.ts
+++ tests/e2e/tests/view/viewcan.2024.2e.test.ts
@@ -161,8 +161,10 @@ async function testCanView(page: Page) {
   await inputAnswerById(page1, '0Q29', '[-5;2]')
   await page1.locator('.bxs-chevron-right').click()
   await page1.getByRole('button', { name: 'Rendre la copie' }).click()
-  await page.waitForTimeout(500)
-  await page1.getByRole('button', { name: 'Terminer' }).click()
+  await page1
+    .getByRole('dialog')
+    .getByRole('button', { name: 'Rendre la copie' })
+    .click()
   logIfVerbose('Accéder aux solutions')
   await page1.getByRole('button', { name: 'Accéder aux solutions' }).click()
   await page1.locator('.bx-toggle-right').click({ button: 'right' })
