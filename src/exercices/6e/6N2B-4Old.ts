d48bf80904557594fdc8804055b8c0483f35af5d
Eric Elter <eric.elter@ac-normandie.fr>
2026-09-25 20:26:40 +0200

    feat : Plus de rigueur dans le référentiel 6N2B

--- src/exercices/6e/6N2B-4Old.ts
+++ src/exercices/6e/6N2B-4Old.ts
@@ -26,20 +26,19 @@ export const titre =
 
 // Gestion de la date de publication initiale
 export const dateDePublication = '04/11/2021'
-export const dateDeModifImportante = '25/09/2026'
 
 /**
  * Presentation didactique : Par combien multiplier un nombre pour que le chiffre des unités devienne le chiffre des ...
  * @author Éric Elter (inspiré par Aude Duvold)
  */
-export const uuid = '1855e'
+export const uuid = '18559'
 
 export const refs = {
-  'fr-fr': ['6N2B-4'],
+  'fr-fr': [],
   'fr-2016': ['6C30-6'],
-  'fr-ch': ['9NO1G-12'],
+  'fr-ch': [],
 }
-export default class MultiplierUnNombreParPuissanceDeDix extends Exercice {
+export default class MultiplierUnNombreParPuissanceDeDixOld extends Exercice {
   constructor() {
     super()
     this.besoinFormulaireCaseACocher = [
@@ -64,7 +63,7 @@ export default class MultiplierUnNombreParPuissanceDeDix extends Exercice {
 
     this.sup = false
     this.sup2 = true
-    this.sup3 = 2
+    this.sup3 = 3
     this.sup4 = true
   }
 
