1a540ec8827acdd5bf5ada4471a83e7545b27c5c
gillesMora <gilles.mora@ac-normandie.fr>
2026-09-26 09:32:38 +0200

    fix(can1SD27-28): modif correction

--- src/exercices/can/1e/can1SD27.ts
+++ src/exercices/can/1e/can1SD27.ts
@@ -43,12 +43,16 @@ export default class FormeCanoniqueXCarrePlusBx extends ExerciceSimple {
       champ1: { value: moitie },
       champ2: { value: carreMoitie },
     }
-    this.consigne = 'Compléter l\'égalité.'
+    this.consigne = "Compléter l'égalité."
     this.question = `x^2${signe}${termeX}=\\left(x${signe}%{champ1}\\right)^2-%{champ2}`
 
     this.correction = `On fait apparaître le début du développement d'une identité remarquable : $x^2${signe}${termeX}=x^2${signe}2\\times ${moitie}\\times x$.<br>
-    Or $\\left(x${signe}${moitie}\\right)^2=x^2${signe}${termeX}+${moitieAuCarre}=x^2${signe}${termeX}+${carreMoitie}$.<br>
-    Il faut donc retrancher $${carreMoitie}$ : $x^2${signe}${termeX}=\\left(x${signe}${miseEnEvidence(moitie)}\\right)^2-${miseEnEvidence(carreMoitie)}$.`
+    On ajoute et on retranche $${moitieAuCarre}$ :<br>
+    $\\begin{aligned}
+    x^2${signe}${termeX}&=\\underbrace{x^2${signe}2\\times ${moitie}\\times x+${moitieAuCarre}}_{\\small\\left(x${signe}${moitie.replace('\\dfrac', '\\frac')}\\right)^2}-${moitieAuCarre}\\\\
+    &=\\left(x${signe}${moitie}\\right)^2-${moitieAuCarre}\\\\
+    &=\\left(x${signe}${miseEnEvidence(moitie)}\\right)^2-${miseEnEvidence(carreMoitie)}
+    \\end{aligned}$`
 
     this.canEnonce = `$x^2${signe}${termeX}=$`
     this.canReponseACompleter = `$\\left(x${signe}\\ldots\\right)^2-\\ldots$`
